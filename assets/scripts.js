4c1680820863ce304e1e057d8a754eea6575c3d1
typeshii-maker <manuelsalom007@gmail.com>
2026-05-12 15:54:29 +0300

    first message

--- assets/scripts.js
+++ assets/scripts.js
@@ -7,3 +7,12 @@ document.getElementsByClassName("DOM_Header")[1].innerHTML="Am in class";
 
 //changing the content on the first paragragh
 document.getElementById("first paragragh").textContent="My DOM is working"
+
+function new_plaintext(){
+    const p =document.createElement('p');
+    p.textContent = "our JS events are working";
+    document.getElementsByTagName('body')[0].appendChild(p);
+}
+function new_plaintext2(){
+    document.getElementsByClassName("change-color")[0].style.color ="red"
+}
