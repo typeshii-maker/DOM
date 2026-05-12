913c8b5ee0c26d199ce1e0334b0e02224d171c01
typeshii-maker <manuelsalom007@gmail.com>
2026-05-12 13:20:59 +0300

    first message

---
+++ assets/scripts.js
@@ -0,0 +1,9 @@
+//get the class
+document.getElementsByClassName("DOM_Header")[0].innerHTML="I am learning DOM and Events";
+
+//change the entire element to a different element
+
+document.getElementsByClassName("DOM_Header")[1].innerHTML="Am in class";
+
+//changing the content on the first paragragh
+document.getElementById("first paragragh").textContent="My DOM is working"
